18d67f2dc6f584d34f8b81af7abc64a3c0adf311
erik schievenin <erik.schievenin@gmail.com>
2022-03-07 22:11:22 +0100

    Exercise complete 3 modes soluctions

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
 console.log('JS OK!')
 
-
 // variabili da configurare
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuExtended = document.querySelector('.header-right');
-const
+// const menuGrill = document.querySelector('.fa-bars');
+// const menuClose = document.querySelector('.close');
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-    // estensione menù griglia al click
 
-    hamburgerMenu.addEventListener('click', function () {
-        hamburgerMenu.style.display = 'block';
-    })
+// // estensione menu griglia al click
 
-// estensione menù griglia al ri-click
+// menuGrill.addEventListener('click', function () {
+//     hamburgerMenu.style.display = 'block';
+// })
 
-hamburgerMenu.addEventListener('click', function () {
-    hamburgerMenu.style.display = 'none';
+// // estensione menù griglia al ri-click
+
+// menuClose.addEventListener('click', function () {
+//     hamburgerMenu.style.display = 'none';
+// })
+
+
+
+// // **************2o METODO*************
+// //variabili da configurare
+
+// const menuGrill = document.querySelector('.fa-bars');
+// const menuClose = document.querySelector('.fa-times');
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// // // estensione menu griglia al click
+
+// menuGrill.addEventListener('click', function () {
+//     hamburgerMenu.classList.add('active');
+// })
+
+// // // estensione menù griglia chiusura
+
+// menuClose.addEventListener('click', function () {
+//     hamburgerMenu.classList.remove('active');
+// })
+
+// **************3o METODO*************
+//variabili da configurare
+
+
+// // estensione menu griglia al click
+
+document.querySelector('.fa-bars').addEventListener('click', function () {
+    document.querySelector('.hamburger-menu').classList.add('active');
+
+    console.log('Tutto ok! Grande bro');
+})
+
+
+// // estensione menù griglia chiusura
+
+document.querySelector('.fa-times').addEventListener('click', function () {
+    document.querySelector('.hamburger-menu').classList.remove('active');
+
+    console.log('Funziona anche questo! Grande bro');
 })
